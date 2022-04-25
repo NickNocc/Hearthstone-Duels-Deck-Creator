e4d5c446c4b97b17d736100cff9870d7b9a68d33
NickNocc <haretakis4@gmail.com>
2022-04-25 12:46:11 -0700

    Update cardsets.js

--- duels-deck-creator/src/utils/cardsets.js
+++ duels-deck-creator/src/utils/cardsets.js
@@ -3,7 +3,8 @@ const axios = require("axios").default;
 const activeSets =
   "journey-to-ungoro,one-night-in-karazhan,whispers-of-the-old-gods,league-of-explorers,voyage-to-the-sunken-city,core,legacy,fractured-in-alterac-valley,united-in-stormwind,forged-in-the-barrens,madness-at-the-darkmoon-faire,scholomance-academy,goblins-vs-gnomes,naxxramas,saviors-of-uldum,kobolds-and-catacombs";
 
-  const hero = "paladin";
+const hero = "paladin";
+
 let fetcher = axios
   .get(
     `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=${activeSets}&class=${hero}&access_token=US5DCIpIr2yLNShXsi5WvdLFNndeLZ5Eek`
