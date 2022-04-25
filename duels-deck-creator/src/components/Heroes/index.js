679928bb5a0af4c06b38df2bd587f241780291ef
NickNocc <haretakis4@gmail.com>
2022-04-25 13:37:28 -0700

    added context

---
+++ duels-deck-creator/src/components/Heroes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import brann from ""
+
+const HeroSelect = (props) => {
+  return (
+    <div className="heroWrapper">
+      <div className="heroSelect">
+        <img src="/Images/Brann/Hero_Brann" alt="Brann"></img>
+      </div>
+    </div>
+  );
+};
+export default HeroSelect;
---
+++ duels-deck-creator/src/context.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from "react";
+// import Brann from "./Images/Brann/Hero_Brann.webp";
+
+export const HeroGallery = React.createContext();
+
+export const GalleryProvider = (props) => {
+
+    const INITIAL_STATE = { buildPhase: false }
+
+    const phaseReducer = (state, action) => {
+        switch (action.type) {
+            case "PHASE":
+                return { buildPhase: !state.buildPhase };
+            default:
+                return state;
+        }
+    }
+
+    const [state, dispatch] = useReducer(phaseReducer, INITIAL_STATE)
+
+
+
+    return (
+        <HeroGallery.Provider value={{state, dispatch}}>
+            {props.children}
+        </HeroGallery.Provider>
+    )
+}
