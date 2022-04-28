03a4a0c4cf3efb5ced4cb3de8fcbeddc882c320d
NickNocc <haretakis4@gmail.com>
2022-04-28 14:15:17 -0700

    Update index.js

--- duels-deck-creator/src/components/Heroes/index.js
+++ duels-deck-creator/src/components/Heroes/index.js
@@ -1,20 +1,5 @@
 import React from "react";
-import brann from "../../Images/Brann/Hero_Brann.webp";
-import demon_hunter from "../../Images/Demon_Hunter/Hero_Stelina.webp";
-import drekthar from "../../Images/DrekThar/Hero_DrekThar.webp"
-import druid from "../../Images/Druid/Hero_Omu.webp"
-import elise from "../../Images/Elise/Hero_Elise.webp"
-import finley from "../../Images/Finley/Hero_Finley.webp"
-import hunter from "../../Images/Hunter/Hero_Slate.webp"
-import mage from "../../Images/Mage/Hero_Mozaki.webp"
-import paladin from "../../Images/Paladin/Hero_Turalyon.webp"
-import priest from "../../Images/Priest/Hero_Illucia.webp"
-import reno from "../../Images/Reno/Hero_Reno.webp"
-import rogue from "../../Images/Rogue/Hero_Lilian.webp"
-import shaman from "../../Images/Shaman/Hero_Fireheart.webp"
-import vanndar from "../../Images/Vanndar/Hero_Vanndar.webp"
-import warlock from "../../Images/Warlock/Hero_Willow.webp"
-import warrior from "../../Images/Warrior/Hero_Rattlegore.webp";
+
 
 const HeroSelect = () => {
   return (
