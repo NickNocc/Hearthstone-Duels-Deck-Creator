863be9c4e65ed6c749d7bc8d26aec02d2fa5b11c
NickNocc <haretakis4@gmail.com>
2022-04-28 14:15:50 -0700

    Update index.js

--- duels-deck-creator/src/components/Heroes/index.js
+++ duels-deck-creator/src/components/Heroes/index.js
@@ -8,7 +8,13 @@ import finley from "../../Images/Finley/Hero_Finley.webp"
 import hunter from "../../Images/Hunter/Hero_Slate.webp"
 import mage from "../../Images/Mage/Hero_Mozaki.webp"
 import paladin from "../../Images/Paladin/Hero_Turalyon.webp"
-
+import priest from "../../Images/Priest/Hero_Illucia.webp"
+import reno from "../../Images/Reno/Hero_Reno.webp"
+import rogue from "../../Images/Rogue/Hero_Lilian.webp"
+import shaman from "../../Images/Shaman/Hero_Fireheart.webp"
+import vanndar from "../../Images/Vanndar/Hero_Vanndar.webp"
+import warlock from "../../Images/Warlock/Hero_Willow.webp"
+import warrior from "../../Images/Warrior/Hero_Rattlegore.webp";
 
 const HeroSelect = () => {
   return (
