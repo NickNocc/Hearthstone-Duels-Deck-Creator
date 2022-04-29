b6224fa013b28305ce7d6ccc18aea0c31267b12e
NickNocc <haretakis4@gmail.com>
2022-04-29 10:19:28 -0700

    Update index.js

--- duels-deck-creator/src/components/Heroes/index.js
+++ duels-deck-creator/src/components/Heroes/index.js
@@ -23,19 +23,7 @@ const HeroSelect = () => {
         <img src={brann} alt="Brann"></img>
         <img src={demon_hunter} alt="Stelina"></img>
         <img src={drekthar} alt="Drek'Thar"></img>
-        <img src={druid} alt="Omu"></img>
-        <img src={elise} alt="Elise"></img>
-        <img src={finley} alt="Finley"></img>
-        <img src={hunter} alt="Slate"></img>
-        <img src={mage} alt="Mozaki"></img>
-        <img src={paladin} alt="Turalyon"></img>
-        <img src={priest} alt="Illucia"></img>
-        <img src={reno} alt="Reno"></img>
-        <img src={rogue} alt="Lilian"></img>
-        <img src={shaman} alt="Fireheart"></img>
-        <img src={vanndar} alt="Vanndar"></img>
-        <img src={warlock} alt="Willow"></img>
-        <img src={warrior} alt="Rattlegore"></img>
+
 
       </div>
     </div>
