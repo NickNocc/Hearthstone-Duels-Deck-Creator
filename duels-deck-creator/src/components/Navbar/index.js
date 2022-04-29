503ea46bab70d82ab48239f761918449b121d052
NickNocc <haretakis4@gmail.com>
2022-04-29 11:27:13 -0700

    Update index.js

--- duels-deck-creator/src/components/Navbar/index.js
+++ duels-deck-creator/src/components/Navbar/index.js
@@ -5,6 +5,8 @@ import HeroPowerDefault from "../../Images/Defaults/HeroPower_Default.webp";
 import CardBackDefault from "../../Images/Defaults/card-back-default.png";
 
 export const Navbar = () => {
+    
+
   return (
     <div className="navWrapper">
       <div className="navMain">
