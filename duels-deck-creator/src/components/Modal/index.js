2634f55e13fe1fba527d3890d51d007b458c404d
NickNocc <haretakis4@gmail.com>
2022-05-02 13:07:10 -0700

    Added styling

--- duels-deck-creator/src/components/Modal/index.js
+++ duels-deck-creator/src/components/Modal/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import HeroSelect from "../Heroes";
+import "./Modal.css";
 
 export const Modal = ({ onClose }) => {
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
         <h3 className="modalTitle"> </h3>
-        
         <HeroSelect />
         <button type="button" onClick={onClose}>
           Close this modal
