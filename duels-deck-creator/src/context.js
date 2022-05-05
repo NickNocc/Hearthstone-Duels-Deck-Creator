f7032df0da5e783b14d570213c09576ff278b693
NickNocc <haretakis4@gmail.com>
2022-05-05 10:56:01 -0700

    Update context.js

--- duels-deck-creator/src/context.js
+++ duels-deck-creator/src/context.js
@@ -1,27 +1,13 @@
 import React, { useReducer } from "react";
+import { Navbar } from "./components/Navbar";
 // import Brann from "./Images/Brann/Hero_Brann.webp";
 
-export const HeroGallery = React.createContext();
+export const heroContext = React.createContext();
 
-export const GalleryProvider = (props) => {
-
-
-    const phaseReducer = (state, action) => {
-        switch (action.type) {
-            case "PHASE":
-                return { buildPhase: !state.buildPhase };
-            default:
-                return state;
-        }
-    }
-
-    const [state, dispatch] = useReducer(phaseReducer, INITIAL_STATE)
-
-
-
+export default function heroes () {
     return (
-        <HeroGallery.Provider value={{state, dispatch}}>
-            {props.children}
-        </HeroGallery.Provider>
+        <heroContext.Consumer>
+            {(hero) => <Navbar hero={heros} />}
+        </heroContext.Consumer>
     )
 }
