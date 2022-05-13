1ff87abbffb0599ca366970c9e4fdc45c0f1077b
NickNocc <haretakis4@gmail.com>
2022-05-12 17:11:46 -0700

    style

--- duels-deck-creator/src/components/Navbar/index.js
+++ duels-deck-creator/src/components/Navbar/index.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { HeroModal } from "../HeroModal";
 
-
 export const Navbar = ({ heroes, currentHero, setCurrentHero }) => {
   // State for modal
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,7 +10,7 @@ export const Navbar = ({ heroes, currentHero, setCurrentHero }) => {
   const [currentHP, setCurrentHP] = useState(currentHero.heroPower);
 
   // State that controls what signature treasure is selected
-  const [currentST, setCurrentST] = useState(currentHero.sigTreasure)
+  const [currentST, setCurrentST] = useState(currentHero.sigTreasure);
 
   // Function that sets current hero power
   const handleChangeHP = (event) => {
@@ -47,16 +46,22 @@ export const Navbar = ({ heroes, currentHero, setCurrentHero }) => {
             alt="Hero Select"
             onClick={toggleModal}
           />
-          <img
-            className="navHP"
-            src={currentHP}
-            alt="Hero Power Select"
-          />
+          <img className="navHP" src={currentHP} alt="Hero Power Select" />
           {/* Hero power selection */}
-          <select className="hpSelect" value={currentHP} onChange={handleChangeHP}>
-            <option value={currentHero.hpSelect.option1}>{currentHero.hpSelect.name1}</option>
-            <option value={currentHero.hpSelect.option2}>{currentHero.hpSelect.name2}</option>
-            <option value={currentHero.hpSelect.option3}>{currentHero.hpSelect.name3}</option>
+          <select
+            className="hpSelect"
+            value={currentHP}
+            onChange={handleChangeHP}
+          >
+            <option value={currentHero.hpSelect.option1}>
+              {currentHero.hpSelect.name1}
+            </option>
+            <option value={currentHero.hpSelect.option2}>
+              {currentHero.hpSelect.name2}
+            </option>
+            <option value={currentHero.hpSelect.option3}>
+              {currentHero.hpSelect.name3}
+            </option>
           </select>
           <img
             className="navST"
@@ -64,13 +69,25 @@ export const Navbar = ({ heroes, currentHero, setCurrentHero }) => {
             alt="Signature Treasure Select"
           />
           {/* Signature treasure selection */}
-          <select value={currentHP} onChange={handleChangeST}>
-            <option value={currentHero.stSelect.option1}>{currentHero.stSelect.name1}</option>
-            <option value={currentHero.stSelect.option2}>{currentHero.stSelect.name2}</option>
-            <option value={currentHero.stSelect.option3}>{currentHero.stSelect.name3}</option>
-            <option value={currentHero.stSelect.option4}>{currentHero.stSelect.name4}</option>
-            <option value={currentHero.stSelect.option5}>{currentHero.stSelect.name5}</option>
-            <option value={currentHero.stSelect.option6}>{currentHero.stSelect.name6}</option>
+          <select value={currentHP} onChange={handleChangeST} className="stSelect">
+            <option value={currentHero.stSelect.option1}>
+              {currentHero.stSelect.name1}
+            </option>
+            <option value={currentHero.stSelect.option2}>
+              {currentHero.stSelect.name2}
+            </option>
+            <option value={currentHero.stSelect.option3}>
+              {currentHero.stSelect.name3}
+            </option>
+            <option value={currentHero.stSelect.option4}>
+              {currentHero.stSelect.name4}
+            </option>
+            <option value={currentHero.stSelect.option5}>
+              {currentHero.stSelect.name5}
+            </option>
+            <option value={currentHero.stSelect.option6}>
+              {currentHero.stSelect.name6}
+            </option>
           </select>
         </div>
       </div>
